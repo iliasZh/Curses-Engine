3c4bf779d70cd904c7edad1d9751b3c573b1d59a
sabazov14@gmail.com <sabazov14@gmail.com>
2021-07-07 23:33:58 +0500

    added simple RNG class

---
+++ Curses Engine/RandomNumberGenerator.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <random>
+#include <cassert>
+#include <vector>
+
+class RNG
+{
+public:
+	RNG()
+		: uid{}
+	{}
+	int UniformIntDist(int min, int max)
+	{
+		assert(min < max);
+		uid.param(std::uniform_int<int>::param_type{ min, max });
+		return uid(rng);
+	}
+private:
+	std::random_device rd{};
+	std::mt19937 rng{ rd() };
+	std::uniform_int_distribution<int> uid;
+};
--- Curses Engine/main.cpp
+++ Curses Engine/main.cpp
@@ -2,10 +2,17 @@
 
 #include "WindowsFunctionality.h"
 #include "Game.h"
+#include "RandomNumberGenerator.h"
+
+#include <iostream>
 
 int main()
 {
-	Game game;
-	Sleep(5000);
+	//Game game;
+	RNG rng;
+	for (int i = 0; i < 100; ++i)
+	{
+		std::cout << rng.UniformIntDist(0, 50) << std::endl;
+	}
 	return 0;
 }
