962d94c84bf53d656ebfd8b7f91101b0b323316f
Ilias <sabazov14@gmail.com>
2021-07-20 18:13:52 +0500

    made keyboard class single-threaded again

--- Console Engine/Keyboard.h
+++ Console Engine/Keyboard.h
@@ -13,21 +13,12 @@ class Keyboard
 {
 public:
 	Keyboard();
-	~Keyboard();
 	bool IsKeyPressed(int vk_code);
 	bool IsKeyPressedOnce(int vkcode);
 
 	bool IsBindingPressed(Controls::Binding b);
 	bool IsBindingPressedOnce(Controls::Binding b);
 private:
-	void PollKeyboard();
-	bool ReadWriteKeystates(bool is_read, int vk_code = 0);
-private:
 	static constexpr size_t num_keys = 256u;
-	std::bitset<num_keys> keystates;
 	std::bitset<num_keys> was_pressed;
-	std::thread poll_thread;
-	mutable std::mutex m;
-	std::chrono::milliseconds polling_period = std::chrono::milliseconds(15u);
-	std::atomic_bool end_polling = false;
 };
